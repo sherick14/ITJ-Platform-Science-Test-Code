9c146c1e26861b3007f5c00f45c390e5a19140e6
sherick14 <68566180+sherick14@users.noreply.github.com>
2023-03-11 19:27:53 -0600

    Navbar component creation and favicon change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@
         <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
+          For help and collaboration with other Vuetify developersssss,
           <br />please join our online
           <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
         </p>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-toolbar class="align-center" color="black">
+      <v-toolbar-title>
+        <img width="134" height="64" :src="AcmeLogo" class="mt-3 ml-6" alt="Acme company logo" />
+      </v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+import AcmeLogo from '@/assets/images/AcmeLogo.png'
+export default {
+  name: 'NavigationBar',
+  data() {
+    return {
+      AcmeLogo: AcmeLogo,
+    }
+  },
+}
+</script>
